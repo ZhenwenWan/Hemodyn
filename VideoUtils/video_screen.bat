df8db50a3ac71aeb7e310cd246acea25c3602ced
NickWanDenmark <aw4wzw@gmail.com>
2025-05-07 16:47:49 +0200

    a note

--- VideoUtils/video_screen.bat
+++ VideoUtils/video_screen.bat
@@ -1,4 +1,4 @@
-ffmpeg -f gdigrab -framerate 30 -offset_x 720 -offset_y 240 -video_size 620x720 -i desktop output.mp4
+ffmpeg -f gdigrab -framerate 30 -offset_x 720 -offset_y 240 -video_size 620x720 -i desktop -t 10 output.mp4
 
 Option	Description
 -f gdigrab	Screen capture on Windows
@@ -7,7 +7,6 @@ Option	Description
 -offset_y	Y start position of the viewport
 -video_size	Width x Height of the capture area
 -i desktop	Capture from the desktop
+-t 10   	Capture 10 seconds
 output.mp4	Output filename
-
-q
 
